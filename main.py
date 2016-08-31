906b625d303f2a4175217ddf4cd4c6db769029d8
William Lau <contact@cwlau.com>
2016-08-31 17:05:36 +0800

    Update as of Aug 31

---
+++ main.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+from datetime import datetime
+import logging
+import MySQLdb
+import os
+import webapp2
+from google.appengine.ext.webapp import template
+from config import MYSQL_DB_CONFIG_APP_ENGINE
+from config import MYSQL_DB_CONFIG_LOCALHOST
+
+
+if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+    _db_config = MYSQL_DB_CONFIG_APP_ENGINE
+else:
+    _db_config = MYSQL_DB_CONFIG_LOCALHOST
+
+
+def totimestamp(dt, epoch=datetime(1970, 1, 1)):
+    td = dt - epoch
+    return td.total_seconds()
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        env = os.getenv('SERVER_SOFTWARE')
+        if (env and env.startswith('Google App Engine/')):
+            # Connecting from App Engine
+            db = MySQLdb.connect(
+                db=_db_config["database"],
+                unix_socket='/cloudsql/berry-cloud-project:us-central1:hongcoin-prod',
+                user='root')
+
+        else:
+            # Connecting from an external network.
+            # Make sure your network is whitelisted
+            db = MySQLdb.connect(
+                db=_db_config["database"],
+                host=_db_config["host"],
+                port=_db_config["port"],
+                user=_db_config["user"],
+                passwd=_db_config["password"]
+            )
+
+
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM `ico_data` ORDER BY `record_datetime` DESC LIMIT 1')
+
+        for r in cursor.fetchall():
+            # logging.info('{}\n'.format(r))
+
+            # result = r
+            update_datetime = r[1]
+            total_ether = r[2]
+            current_tier = r[6]
+            tokens_current_tier = r[7]
+            total_tokens_sold = r[8]
+
+        # process result values
+        # display in UI
+
+        template_values = {
+            "update_datetime": update_datetime,
+            "total_ether": total_ether,
+            "total_tokens_sold": total_tokens_sold,
+            "total_tokens_price": 1 + (5 * current_tier) / 100.0,
+            "tokens_current_tier": tokens_current_tier,
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'template/home.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+
+class StatsHandler(webapp2.RequestHandler):
+    def get(self):
+        env = os.getenv('SERVER_SOFTWARE')
+        if (env and env.startswith('Google App Engine/')):
+            # Connecting from App Engine
+            db = MySQLdb.connect(
+                db=_db_config["database"],
+                unix_socket='/cloudsql/berry-cloud-project:us-central1:hongcoin-prod',
+                user='root')
+
+        else:
+            # Connecting from an external network.
+            # Make sure your network is whitelisted
+            db = MySQLdb.connect(
+                db=_db_config["database"],
+                host=_db_config["host"],
+                port=_db_config["port"],
+                user=_db_config["user"],
+                passwd=_db_config["password"]
+            )
+
+        cursor = db.cursor()
+        cursor.execute(
+            "SELECT * FROM `ico_data` WHERE "
+            " `record_datetime` >= NOW() - INTERVAL 1 DAY "
+            "AND ((`record_datetime` like '%:00:%') or (`record_datetime` like '%:30:%')) ;")
+
+        result = []
+        for r in cursor.fetchall():
+            result.append({
+                "time": totimestamp(r[1]) * 1000,
+                "ether": r[2],
+                "current_tier_remaining": r[7],
+                "tokens_issued": r[8]
+            })
+            # logging.info('{}'.format(r))
+
+
+        template_values = {
+            "result": result
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'template/stats.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+class HowItWorksHandler(webapp2.RequestHandler):
+    def get(self):
+        self.redirect("/")
+
+
+class FAQHandler(webapp2.RequestHandler):
+    def get(self):
+        self.redirect("/")
+
+
+class LetsEncryptHandler(webapp2.RequestHandler):
+
+    def get(self, challenge):
+        self.response.headers['Content-Type'] = 'text/plain'
+        responses = {
+            'abc': '123',
+            'def': '456'
+        }
+        self.response.write(responses.get(challenge, ''))
+
+
+class NotFoundPageHandler(webapp2.RequestHandler):
+    def get(self):
+        logging.info("NotFoundPageHandler triggered from " + os.path.basename(__file__))
+        self.error(404)
+        template_values = {}
+
+        path = os.path.join(os.path.dirname(__file__), 'template/404.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/stats', StatsHandler),
+    ('/faq', FAQHandler),
+    ('/how-it-works', HowItWorksHandler),
+    ('/api', HowItWorksHandler),
+    ('/.well-known/acme-challenge/([\w-]+)', LetsEncryptHandler),
+    ('/.*', NotFoundPageHandler),
+], debug=True)
+
---
+++ nodejs/HongContractDataServer.js
@@ -0,0 +1,132 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var dateFormat = require('dateformat');
+
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:8545'));
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+
+var options = require('./options');
+var favicon = require('serve-favicon');
+
+var MyContract;
+var myContractInstance;
+var myContractInstanceAddress;
+
+
+var mysql = require('mysql');
+
+function handleError (err) {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            connect();
+        } else {
+            console.error(err.stack || err);
+        }
+    }
+}
+
+function connect () {
+    connection = mysql.createConnection({
+        host    : options.storageConfig.host,
+        user    : options.storageConfig.user,
+        password: options.storageConfig.password,
+        database: options.storageConfig.database
+    });
+    connection.connect(handleError);
+    connection.on('error', handleError);
+}
+
+
+var mysql = require('mysql');
+var connection;
+connect();
+
+app.use(favicon(__dirname + '/favicon.ico'));
+
+
+
+app.get('/', function(req, res){
+    console.log('GET /')
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello world');
+});
+
+app.get('/api/record', function(req, res){
+    // redirect handler for contract form
+    contract_abi = options.storageConfig.contractAbi;
+    contract_address = options.storageConfig.contractAddr;
+
+    // console.log(contract_abi)
+    // console.log(contract_address)
+
+    contract_obj = web3.eth.contract(contract_abi);
+    hong = contract_obj.at(contract_address);
+
+    var contractBalance = web3.eth.getBalance(contract_address);
+    var returnWalletBalance = web3.eth.getBalance(hong.returnWallet());
+    var rewardWalletBalance = web3.eth.getBalance(hong.rewardWallet());
+    var extraBalanceWalletBalance = web3.eth.getBalance(hong.extraBalanceWallet());
+    var managementFeeWalletBalance = web3.eth.getBalance(hong.managementFeeWallet());
+    var contractString = JSON.stringify(web3.eth.getStorageAt(contract_address));
+
+    var totalContractBalance = web3.fromWei(contractBalance, "ether").toNumber()
+                             + web3.fromWei(extraBalanceWalletBalance, "ether").toNumber()
+                             + web3.fromWei(returnWalletBalance, "ether").toNumber()
+                             + web3.fromWei(rewardWalletBalance, "ether").toNumber()
+                             + web3.fromWei(managementFeeWalletBalance, "ether").toNumber();
+
+
+    var query = "INSERT INTO `ico_data` (`record_datetime`, `balance_total_eth`, `balance_main_eth`, "
+                + "`balance_mgmtfee_eth`, `balance_extra_eth`,"
+                + "`current_tier`, `token_available_current_tier`, `total_tokens_issued`, `bounty_issued`)"
+                + "VALUES (NOW(), ?, ?, ?, ?, ?, ?, ?, ?);";
+
+    console.log(query);
+    console.log([
+            totalContractBalance, web3.fromWei(contractBalance, "ether").toNumber(),
+            web3.fromWei(managementFeeWalletBalance, "ether").toNumber(),
+            web3.fromWei(extraBalanceWalletBalance, "ether").toNumber(),
+            hong.getCurrentTier().toNumber(), hong.tokensAvailableAtCurrentTier().toNumber(), hong.tokensCreated().toNumber(),
+            hong.bountyTokensCreated().toNumber()
+    ]);
+
+    connection.query(
+        query, [
+            totalContractBalance, web3.fromWei(contractBalance, "ether").toNumber(),
+            web3.fromWei(managementFeeWalletBalance, "ether").toNumber(),
+            web3.fromWei(extraBalanceWalletBalance, "ether").toNumber(),
+            hong.getCurrentTier().toNumber(), hong.tokensAvailableAtCurrentTier().toNumber(), hong.tokensCreated().toNumber(),
+            hong.bountyTokensCreated().toNumber()
+        ]
+    , function(err, rows, fields) {
+        if (err) throw err;
+    });
+
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Done');
+});
+
+
+app.post('/', function(req, res){
+    console.log('POST /');
+    console.dir(req.body);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Thanks for trying, but nothing here :(');
+});
+
+app.get('*', function(req, res){
+    console.log('Not found')
+    res.writeHead(400, {'Content-Type': 'text/html'});
+    res.end("Page not found");
+});
+
+port = 5050;
+app.listen(port);
+console.log('Listening at http://localhost:' + port)
